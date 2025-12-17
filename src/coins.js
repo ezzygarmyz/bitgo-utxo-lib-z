3c22791af20f1ed3053f4c2fb75e355eaaf5570e
ezzygarmyz <dragkhai@gmail.com>
2025-12-18 00:59:12 +0100

    support Z coins

--- src/coins.js
+++ src/coins.js
@@ -2,33 +2,7 @@
 const typeforce = require('typeforce')
 
 const coins = {
-  BCH: 'bch',
-  BSV: 'bsv',
-  BTC: 'btc',
-  BTG: 'btg',
-  LTC: 'ltc',
   ZEC: 'zec',
-  DASH: 'dash'
-}
-
-coins.isBitcoin = function (network) {
-  return typeforce.value(coins.BTC)(network.coin)
-}
-
-coins.isBitcoinCash = function (network) {
-  return typeforce.value(coins.BCH)(network.coin)
-}
-
-coins.isBitcoinSV = function (network) {
-  return typeforce.value(coins.BSV)(network.coin)
-}
-
-coins.isBitcoinGold = function (network) {
-  return typeforce.value(coins.BTG)(network.coin)
-}
-
-coins.isLitecoin = function (network) {
-  return typeforce.value(coins.LTC)(network.coin)
 }
 
 coins.isZcash = function (network) {
@@ -36,11 +10,6 @@ coins.isZcash = function (network) {
 }
 
 coins.isValidCoin = typeforce.oneOf(
-  coins.isBitcoin,
-  coins.isBitcoinCash,
-  coins.isBitcoinSV,
-  coins.isBitcoinGold,
-  coins.isLitecoin,
   coins.isZcash
 )
 
--- src/networks.js
+++ src/networks.js
@@ -3,78 +3,6 @@
 var coins = require('./coins')
 
 module.exports = {
-  dash: {
-    messagePrefix: '\x19DarkCoin Signed Message:\n',
-    bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
-    },
-    pubKeyHash: 0x4c, // https://dash-docs.github.io/en/developer-reference#opcodes
-    scriptHash: 0x10,
-    wif: 0xcc,
-    coin: coins.DASH
-  },
-  dashTest: {
-    messagePrefix: '\x19DarkCoin Signed Message:\n',
-    bip32: {
-      public: 0x043587cf,
-      private: 0x04358394
-    },
-    pubKeyHash: 0x8c, // https://dash-docs.github.io/en/developer-reference#opcodes
-    scriptHash: 0x13,
-    wif: 0xef, // https://github.com/dashpay/godashutil/blob/master/wif.go#L72
-    coin: coins.DASH
-  },
-  bitcoincash: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bech32: 'bc',
-    bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
-    },
-    pubKeyHash: 0x00,
-    scriptHash: 0x05,
-    wif: 0x80,
-    coin: coins.BCH,
-    forkId: 0x00
-  },
-  bitcoincashTestnet: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bech32: 'tb',
-    bip32: {
-      public: 0x043587cf,
-      private: 0x04358394
-    },
-    pubKeyHash: 0x6f,
-    scriptHash: 0xc4,
-    wif: 0xef,
-    coin: coins.BCH
-  },
-  bitcoinsv: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bech32: 'bc',
-    bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
-    },
-    pubKeyHash: 0x00,
-    scriptHash: 0x05,
-    wif: 0x80,
-    coin: coins.BSV,
-    forkId: 0x00
-  },
-  bitcoinsvTestnet: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bech32: 'tb',
-    bip32: {
-      public: 0x043587cf,
-      private: 0x04358394
-    },
-    pubKeyHash: 0x6f,
-    scriptHash: 0xc4,
-    wif: 0xef,
-    coin: coins.BSV
-  },
   zcash: {
     messagePrefix: '\x18ZCash Signed Message:\n',
     bech32: 'bc',
@@ -95,37 +23,6 @@ module.exports = {
     },
     coin: coins.ZEC
   },
-  zcashTest: {
-    messagePrefix: '\x18ZCash Signed Message:\n',
-    bech32: 'tb',
-    bip32: {
-      public: 0x043587cf,
-      private: 0x04358394
-    },
-    pubKeyHash: 0x1d25,
-    scriptHash: 0x1cba,
-    wif: 0xef,
-    consensusBranchId: {
-      1: 0x00,
-      2: 0x00,
-      3: 0x5ba81b19,
-      4: 0x76b809bb
-    },
-    coin: coins.ZEC
-  },
-  bitcoingold: {
-    messagePrefix: '\x18Bitcoin Gold Signed Message:\n',
-    bech32: 'btg',
-    bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
-    },
-    pubKeyHash: 0x26,
-    scriptHash: 0x17,
-    wif: 0x80,
-    coin: coins.BTG,
-    forkId: 0x4F /* 79 */
-  },
   bitcoinz: {
     messagePrefix: '\x18BitcoinZ Signed Message:\n',
     bech32: 'bc',
@@ -136,8 +33,6 @@ module.exports = {
     pubKeyHash: 0x1cb8,
     scriptHash: 0x1cbd,
     wif: 0x80,
-    // This parameter was introduced in version 3 to allow soft forks, for version 1 and 2 transactions we add a
-    // dummy value.
     consensusBranchId: {
       1: 0x00,
       2: 0x00,
@@ -147,16 +42,15 @@ module.exports = {
     coin: coins.ZEC,
     isZcash: true
   },
-  bitcoinzTest: {
-    messagePrefix: '\x18BitcoinZ Signed Message:\n',
-    bech32: 'tb',
+  litecoinz: {
+    messagePrefix: '\x18LitecoinZ Signed Message:\n',
     bip32: {
-      public: 0x043587cf,
-      private: 0x04358394
+      public: 0x0488b21e,
+      private: 0x0488ade3
     },
-    pubKeyHash: 0x1d25,
-    scriptHash: 0x1cba,
-    wif: 0xef,
+    pubKeyHash: 0x0ab3,
+    scriptHash: 0x0ab8,
+    wif: 0x80,
     consensusBranchId: {
       1: 0x00,
       2: 0x00,
@@ -166,39 +60,23 @@ module.exports = {
     coin: coins.ZEC,
     isZcash: true
   },
-  bitcoin: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
+  zeroclassic: {
+    messagePrefix: '\x18Zero Signed Message:\n',
     bech32: 'bc',
     bip32: {
       public: 0x0488b21e,
       private: 0x0488ade4
     },
-    pubKeyHash: 0x00,
-    scriptHash: 0x05,
+    pubKeyHash: 0x1cb8,
+    scriptHash: 0x1cbd,
     wif: 0x80,
-    coin: coins.BTC
-  },
-  testnet: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bech32: 'tb',
-    bip32: {
-      public: 0x043587cf,
-      private: 0x04358394
+    consensusBranchId: {
+      1: 0x00,
+      2: 0x00,
+      3: 0x5c7b7d2f,
+      4: 0x7a737763
     },
-    pubKeyHash: 0x6f,
-    scriptHash: 0xc4,
-    wif: 0xef,
-    coin: coins.BTC
-  },
-  litecoin: {
-    messagePrefix: '\x19Litecoin Signed Message:\n',
-    bip32: {
-      public: 0x019da462,
-      private: 0x019d9cfe
-    },
-    pubKeyHash: 0x30,
-    scriptHash: 0x32,
-    wif: 0xb0,
-    coin: coins.LTC
+    coin: coins.ZEC,
+    isZcash: true
   }
 }
